ddc5d3f9b909eebf5bb9d21bddbfbb33d6d8c486
Scott Kurz <skurz@us.ibm.com>
2020-11-14 17:19:32 -0500

    cp app-deploy.yaml from src->output location as well
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- build.sh
+++ build.sh
@@ -10,5 +10,8 @@ DEVFILE_DEPLOY_YAML_LOC="${DEVFILE_DEPLOY_YAML_LOC:-https://raw.githubuserconten
 sed -e "s!{{.OL_DRIVER_VERSION}}!$OL_DRIVER_VERSION!; s!{{.OL_APP_STACK_IMAGE}}!$OL_APP_STACK_IMAGE!; s!{{.DEVFILE_DOCKERFILE_LOC}}!$DEVFILE_DOCKERFILE_LOC!; s!{{.DEVFILE_DEPLOY_YAML_LOC}}!$DEVFILE_DEPLOY_YAML_LOC!" src/devfile.yaml > devfile.yaml
 sed -e "s!{{.BASE_OS_IMAGE}}!$BASE_OS_IMAGE!; s!{{.OL_DRIVER_VERSION}}!$OL_DRIVER_VERSION!" src/stackimage/Dockerfile > stackimage/Dockerfile
 
-# Outer loop customization.
-sed -e "s!{{.OL_APP_STACK_IMAGE}}!$OL_APP_STACK_IMAGE!; s!{{.OL_UBI_IMAGE}}!$OL_UBI_IMAGE!" src/outer-loop/latest/Dockerfile > outer-loop/latest/Dockerfile
+# Outer loop customization of Dockerfile
+sed -e "s!{{.OL_APP_STACK_IMAGE}}!$OL_APP_STACK_IMAGE!; s!{{.OL_UBI_IMAGE}}!$OL_UBI_IMAGE!" src/outer-loop/Dockerfile > outer-loop/latest/Dockerfile
+
+# Outer loop copy of app-deploy.yaml (no customization at present)
+cp src/outer-loop/app-deploy.yaml outer-loop/latest/app-deploy.yaml
